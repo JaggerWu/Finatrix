b23cfbf26e41fcecf7bdbbb823e42aebf8cc93b8
Jian Wu <xcb479@alumni.ku.dk>
2019-10-16 23:45:23 +0200

    Add project files.

---
+++ Finatrix/Finatrix.cpp
@@ -0,0 +1,12 @@
+﻿// Finatrix.cpp : Defines the entry point for the application.
+//
+
+#include "Finatrix.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
